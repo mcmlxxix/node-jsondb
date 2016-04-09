37ddd0fa78ff4db4f56a2b854c69be1696aad1e9
MJ <mcmlxxix@users.noreply.github.com>
2016-04-09 13:44:01 -0400

    filling in gaps
    added code for subscriptions, locks, updates

--- node-jsondb.js
+++ node-jsondb.js
@@ -1,7 +1,7 @@
 /**
 	DATABASE PACKET STRUCTURE
-		query={
-			id: <request id>,
+		request={
+			id: <client id>,
 			db: <database name>,
 			oper: <db operation>,
 			data: <see below>
@@ -40,9 +40,6 @@
 
 var fs = require('fs');
 var jp = require('node-jpath');
-
-const rxTokens = /([A-Za-z0-9_\*@\$\(\)]+(?:\[.+?\])?)/g;
-const delimiter = "/";
 
 /* database object definition */
 function JSONdb(name) {
@@ -101,12 +98,14 @@ JSONdb.prototype.isLocked = function(request,callback) {
 function handleRequest(request,func,callback) {
 	if(request.id == null)
 		request.id = "L" + this.counter++;
+	if(request.time == null)
+		request.time = process.hrtime().toString();
 	var response = [];
 	for(var i=0;i<request.data.length;i++) {
-		response = response.concat(func.call(this,request.data[i],request.id));
+		response = response.concat(func.call(this,request.data[i],request.id,callback));
 	}
 	if(typeof callback == "function")
-		callback(request,response);
+		callback(response);
 	return response;
 }
 function read(query,clientID) {
@@ -117,27 +116,64 @@ function write(query,clientID) {
 }
 function lock(query,clientID) {
 	var path = getPath(query.path);
-	var lock = getLock.call(this,query);
-	if(lock == null) {
-		this.metadata[query.path] = query.lock;
+	var data = getMetadata.call(this,query);
+	if(data.length == 0) {
+		this.metadata[query.path] = newMetadata();
+		this.metadata[query.path].lock[clientID] = query.lock;
+		data.push(this.metadata[query.path]);
 	}
 	else {
-		
+		for(var i=0;i<data.length;i++) {
+			
+		}
 	}
-	query.lock = lock;
+	query.data = data;
 	return query;
 }
 function unlock(query,clientID) {
-	
+	var path = getPath(query.path);
+	if(this.metadata[path] != null) {
+		delete this.metadata[path].lock[clientID];
+		var data = getMetadata(path);
+		for(var i=0;i<data.length;i++) {
+			for(var s in data[i].subscribe) {
+				var callback = data[i].subscribe[s];
+				if(typeof callback == "function") {
+					callback(query);
+				}
+			}
+		}
+	}
+	query.data = this.metadata[path];
+	return query;
 }
-function subscribe(query,clientID) {
-	
+function subscribe(query,clientID,callback) {
+	var path = getPath(query.path);
+	if(this.metadata[path] == null) {
+		this.metadata[path] = newMetadata();
+	}
+	this.metadata[path].subscribe[clientID] = callback;
+	query.data = this.metadata[path];
+	return query;
 }
 function unsubscribe(query,clientID) {
-	
+	var path = getPath(query.path);
+	if(this.metadata[path] != null) {
+		delete this.metadata[path].subscribe[clientID];
+	}
+	query.data = this.metadata[path];
+	return query;
 }
 function isSubscribed(query,clientID) {
-	
+	var path = getPath(query.path);
+	var data = getMetadata.call(this,query);
+	if(data.length == 0) {
+		query.value = false;
+	}
+	else {
+		query.value = true;
+	}
+	query.data = data;
 }
 function isLocked(query,clientID) {
 	var path = getPath(query.path);
@@ -148,14 +184,21 @@ function isLocked(query,clientID) {
 function getPath(path) {
 	return path.match(/([A-Za-z0-9_\*@\$\(\)]+(?:\[.+?\])?)/g).join("/");
 }
-function getLock(path) {
+function getMetadata(path) {
+	var data = [];
 	for(var j in this.metadata) {
-		/* if a child or parent of this query path is locked */
+		/* if a child or parent of this query path matches existing metadata */
 		if(j.match(path) || path.match(j)) {
-			return this.metadata[j];
+			data.push(this.metadata[j]);
 		}
 	}
-	return null;
+	return data;
+}
+function newMetadata() {
+	return {
+		lock:{},
+		subscribe:{}
+	};
 }
 function log(str) {
 	console.log('jsondb: ' + str);
